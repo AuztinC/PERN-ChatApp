338006037f9985ed83d2756ebbfe0ccad2986e5e
Austin Cripe <aaustinc@live.com>
2023-10-29 19:52:17 -0400

    socket running

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,33 @@ const express = require('express')
 const app = express()
 const { createServer } = require('node:http')
 const { join } = require('node:path')
+const { isNullOrUndefined } = require('node:util')
 const { Server } = require('socket.io')
 
-const server = createServer(app)
-const io = new Server(server)
+let users = []
 
-io.on('connection', (socket)=>{
-    console.log(socket)
+const server = createServer(app)
+const io = new Server(server, {
+    pingTimeout: 60000,
+    cors:{
+        origin: "http://localhost:5174",
+    }
 })
 
-server.listen(process.env.PORT || 3000, () =>{
-    console.log('express running')
+app.use('/', (req, res)=>{
+    
+})
+
+io.on('connection', (socket)=>{
+    users.push(socket)
+})
+
+io.on('disconnect', (socket)=>{
+    users.find(user=>user.id===socket.id)
+})
+
+const PORT = process.env.PORT || 3000
+
+server.listen(PORT, () =>{
+    console.log(`${PORT}`)
 })
